97880f3f2aec8492bcfdc67407d81e7772b979c3
annaAsta <137793849+annaAsta@users.noreply.github.com>
2023-07-05 15:36:37 +0400

    Update function.rs

--- ch3/src/function.rs
+++ ch3/src/function.rs
@@ -1,24 +1,6 @@
 fn main() {
-    // print_labeled_measurement(5, 'h');
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
 
-    //операторы и вырожения
-    // let y = {
-    //     let x = 3;
-    //     x + 1
-    // };
-    //
-    // println!("The value of y is: {y}");
-
-    let x = plus_one(5);
-
-    println!("The value of x is: {x}");
-}
-
-// функция
-// fn print_labeled_measurement(value: i32, unit_label: char) {
-//     println!("The measurement is: {value}{unit_label}");
-// }
-
-fn plus_one(x: i32) -> i32 {
-    x + 1
+    println!("The value of number is: {number}");
 }
